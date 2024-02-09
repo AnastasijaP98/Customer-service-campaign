d1d27a73bcdaf0205cab3dc90e5c2b402d94ac83
AnastasijaP98 <159533159+AnastasijaP98@users.noreply.github.com>
2024-02-09 12:13:15 +0100

    dailyCustomerAddAPI

---
+++ dailyCustomerAddAPI/Program.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Net.Http.Headers;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddAuthentication().AddJwtBearer();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseCors();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseCors();
+app.UseAuthentication();
+app.UseAuthorization();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+
+app.MapPost("/FileUpload", async (IFormFile file) =>
+{
+    string tempfile = CreateTempfilePath();
+    using var stream = File.OpenWrite(tempfile);
+    await file.CopyToAsync(stream);
+
+    
+});
+
+app.MapPost("/FileUploadMany", async (IFormFileCollection myFiles) =>
+{
+    foreach (var file in myFiles)
+    {
+        string tempfile = CreateTempfilePath();
+        using var stream = File.OpenWrite(tempfile);
+        await file.CopyToAsync(stream);
+
+        
+    }
+});
+
+static string CreateTempfilePath()
+{
+    var filename = $"{Guid.NewGuid()}.tmp";
+    var directoryPath = Path.Combine("Files", "uploads");
+    if (!Directory.Exists(directoryPath)) Directory.CreateDirectory(directoryPath);
+
+    return Path.Combine(directoryPath, filename);
+}
+
+
+app.Run();
+
+
+
+
